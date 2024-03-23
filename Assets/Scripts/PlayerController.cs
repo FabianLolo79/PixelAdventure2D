00e1d345806db24c2f2508e6fc8f3236a6e7f5bd
Fabián E. García <fabianegl79@gmail.com>
2024-03-23 19:30:41 -0300

    Prefabs de frutas y comportamiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,15 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D _rbPlayer;
 
+    //Variables para el salto mejorado
+
+    public bool betterJump = false;
+
+    public float fallMultiplier = 0.5f;
+
+    public float lowJumpMultiplier = 1.0f;
+
+
     private void Awake()
     {
         _rbPlayer = GetComponent<Rigidbody2D>();
@@ -36,6 +45,19 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey("space") && Checkground.isGrounded)
         {
             _rbPlayer.velocity = new Vector2(_rbPlayer.velocity.x, JumpSpeed);
+        }
+
+        if(betterJump)
+        {
+            if (_rbPlayer.velocity.y < 0)
+            {
+                _rbPlayer.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+
+            if (_rbPlayer.velocity.y > 0 && !Input.GetKey("space"))
+            {
+                _rbPlayer.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
         }
     }
 }
